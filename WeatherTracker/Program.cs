654248c5b5386f98b8b90a9cec98c8211927fac7
Justin Huth <Jayhuth96@gmail.com>
2023-03-16 09:41:23 -0500

    Completed the excercise

---
+++ WeatherTracker/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json.Linq;
+
+namespace WeatherTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+          
+            var city = "Oklahoma City";
+
+            var apiKey = "87ea734b11c7be6b0ce70dde4b08cc32";
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apiKey}";
+
+            var response = client.GetStringAsync(weatherURL).Result;
+           
+            var formattedResponse = JObject.Parse(response).GetValue("main");
+            
+            Console.WriteLine(formattedResponse);
+        }
+    }
+}
